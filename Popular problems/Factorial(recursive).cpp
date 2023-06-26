7784bc7f261e4948e47a99c536f92f009b7ab2cc
Ulvi Sadigzada <57990906+ulvisadigzada@users.noreply.github.com>
2023-06-26 15:07:21 +0400

    Update Factorial(recursive).cpp

--- Popular problems/Factorial(recursive).cpp
+++ Popular problems/Factorial(recursive).cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int n)
+{
+    if(n==0)
+        return 1;
+    else
+        return fact(n-1)*n;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin>>n;
+
+    cout<<fact(n);
+}
 
